691ed267fb2ab886b293d31be685dd189169a925
trawais002 <trawais002@gmail.com>
2016-12-19 21:27:09 +0100

    form for adding visitor is now on one line

--- src/AppBundle/Form/ReservationSlotType.php
+++ src/AppBundle/Form/ReservationSlotType.php
@@ -13,7 +13,11 @@ class ReservationSlotType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('date', DateTimeType::class, ['label' => 'Kdy: '])
+            ->add('date', DateTimeType::class, ['label' => 'Kdy: ',
+                'date_widget' => 'single_text',
+                'time_widget' => 'choice',
+                'minutes' => [0,5,10,15,20,25,30,35,40,45,50,55]
+            ])
             ->add('location', TextType::class, ['label' => 'Kde: ', 'required' => false])
             ->add('lector', TextType::class, ['label' => 'S kym: ', 'required' => false])
             ->add('level', TextType::class, ['label' => 'Pro koho: ', 'required' => false])
